38b43f865a5c694ccfd16cf465580eb034ef30a1
Ikuo Kumagai <i-kumagai@bit-isle.co.jp>
2014-10-29 16:16:21 +0900

    Update rdo_trial.sh

--- rdo_trial.sh
+++ rdo_trial.sh
@@ -6,7 +6,6 @@ cd packstack
 python setup.py install_puppet_modules
 bin/packstack --gen-answer-file=answers
 cp -p answers answers.org
-#sed -i 's/CONFIG_AMQP_SERVER=qpid/CONFIG_AMQP_SERVER=rabbitmq/' answers
 sed -i 's/CONFIG_USE_EPEL=n/CONFIG_USE_EPEL=y/' answers
 sed -i s/CONFIG_COMPUTE_HOSTS=.*$/CONFIG_COMPUTE_HOSTS=${COMHOSTS}/ answers 
 
@@ -15,11 +14,4 @@ sed -i s/CONFIG_NOVA_NETWORK_PUBIF=.*$/CONFIG_NOVA_NETWORK_PUBIF=bond1/ answers
 sed -i s/CONFIG_NOVA_NETWORK_PRIVIF=.*$/CONFIG_NOVA_NETWORK_PRIVIF=bond0/ answers
 sed -i s/CONFIG_PROVISION_DEMO_FLOATRANGE=.*$/CONFIG_PROVISION_DEMO_FLOATRANGE=${FLRANGE}/ answers
 bin/packstack --answer-file=answers
-
-
-
-#    1  b0ip=`ifconfig bond0|grep "inet addr"|awk -F '[ :]*' '{print $4}'`
-#    2  echo $b0ip
-#    3  b1ip=`ifconfig bond1|grep "inet addr"|awk -F '[ :]*' '{print $4}'`
-#openstack-neutron-ml2.noarch 0:2014.1.3-1.el6
 
